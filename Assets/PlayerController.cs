462dc176f15185815c619bbee9164d8687d7227b
amathews4 <amathews4@ggc.edu>
2021-09-06 21:55:53 -0400

    Starting Scene

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody myBod;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBod = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        //transform.position += (new Vector3(h, 0, 0)) * Time.deltaTime * 5;
+        myBod.velocity = new Vector3(h * 5, myBod.velocity.y,myBod.velocity.z); //change x only
+    }
+}
