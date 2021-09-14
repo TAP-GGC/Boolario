f5836860828e5edc1043548c3c8fe2a615fe3681
kenixon01 <knixon4@ggc.edu>
2021-09-14 16:34:56 -0400

    adds CameraFollow.cs script to Main Camera

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Transform playerTransform; //Transform class manipulates the position, scale, and rotation of Unity objects
+
+    void Start() 
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform; //creates a variable to hold the player object named "Boolrio"
+    }
+
+    void LateUpdate()
+    {
+        Vector3 currentCameraPosition = transform.position; //variable to hold the camera's current position
+        currentCameraPosition.x = playerTransform.position.x; //sets the current x-position of the camera to the x-position of the player
+        currentCameraPosition.y = playerTransform.position.y + 1; //sets the current y-position of the camera to the y-position of the player
+        transform.position = currentCameraPosition; //stores the temporary position of the camera into the actual position
+    }
+}
